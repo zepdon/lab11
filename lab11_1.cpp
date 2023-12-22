52ec81b4fccfe755d4c69dd165f66fce61f49f5c
Poon <zeepengineer@gmail.com>
2023-12-22 15:13:48 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,42 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+    #include<iostream>
+    #include<cstdlib>
+    #include<ctime>
+
+    using namespace std;
+
+    int main(){
+        cout << "Press Enter 3 times to reveal your future.";
+        cin.ignore();
+        cin.ignore();
+        cin.ignore();
+        srand(time(0));
+        int x=rand()%10;
+        if (x==0 || x==1)
+        {
+            cout << "You will get A in this 261102.";
+        }else if (x==2)
+        {
+            cout << "You will get B+ in this 261102.";
+        }else if (x==3)
+        {
+            cout << "You will get B in this 261102.";
+        }else if (x==4)
+        {
+            cout << "You will get C+ in this 261102.";
+        }else if (x==5)
+        {
+            cout << "You will get C in this 261102.";
+        }else if (x==6)
+        {
+            cout << "You will get D+ in this 261102.";
+        }else if (x==7)
+        {
+            cout << "You will get D in this 261102.";
+        }else if (x==8)
+        {
+            cout << "You will get F in this 261102.";
+        }else if (x==9)
+        {
+            cout << "You will get W in this 261102.";
+        }
+    }
